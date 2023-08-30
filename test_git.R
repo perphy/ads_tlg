37a30019f907a8e53b0cad15e9bd07cf27cf075a
perphy <perphyzhao@163.com>
2023-08-30 17:43:49 +0800

    zz

--- test_git.R
+++ test_git.R
@@ -1,17 +1,4 @@
 x <- 1
 y=2
-<<<<<<< HEAD
-vv
-=======
-z <- 3
-<<<<<<< HEAD
->>>>>>> 1a54ae7009cf4ce39aaa97018233e0206a558120
-=======
-a <- 1
-<<<<<<< HEAD
->>>>>>> 04825acd28ca2367ccfc648b9194b4bf879fb0ce
-=======
 
-irisx <- iris 
->>>>>>> 8a48dcf46cde87fa23e51da152efbf5e5065a6c9
 b <- z
